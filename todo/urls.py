179c46c4916552f86db98331cb0fdeb8bde74a9a
Tamil Selvan <gayathree.tamil-selvan.ext@wts.com>
2023-12-02 12:46:52 +0530

    modify tasks

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # add task
     path('addTask/', views.addTask, name = 'addTask'),
-
+    # mark as done
+    path('markAsDone/<int:pk>/', views.markAsDone, name = 'markAsDone'),
+    # mark as undone
+    path('markAsUndone/<int:pk>/',views.markAsUndone, name='markAsUndone'),
+    # edit feature
+    path('editTask/<int:pk>/', views.editTask, name='editTask'),
+    # delete task
+    path('deleteTask/<int:pk>/', views.deleteTask, name='deleteTask')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
@@ -7,4 +7,33 @@ def addTask(req):
     task = req.POST['task']
     Task.objects.create(task = task)
     # return HttpResponse('The form is submitted')
+    return redirect('home')
+def markAsDone(req, pk):
+    # return HttpResponse(pk)
+    task = get_object_or_404(Task, id=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+    # return HttpResponse(task)
+def markAsUndone(req, pk):
+    task = get_object_or_404(Task, id=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+def editTask(req, pk):
+    task = get_object_or_404(Task, id = pk)
+    if req.method == 'POST':
+        new_task = req.POST['task']
+        print(new_task)
+        task.task = new_task
+        task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': task
+        }
+        return render(req, 'editTask.html', context)
+def deleteTask(req, pk):
+    task = get_object_or_404(Task, id = pk)
+    task.delete()
     return redirect('home')
